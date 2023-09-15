0653693e2c9fd69b46dc643b9ed165f6ad74db76
erick.aulestia <erick.aulestia@meduca.gob.pa>
2023-09-15 09:47:30 -0500

    added home, add-news

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddNewsComponent } from './news/add-news/add-news.component';
+import { HomeComponent } from './home/home.component';
+import { HomeNewsComponent } from './news/home-news/home-news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path: 'news',
+    component: HomeNewsComponent
+  },
+  {
+    path: 'news/add',
+    component: AddNewsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddNewsComponent } from './news/add-news/add-news.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { HomeNewsComponent } from './news/home-news/home-news.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddNewsComponent,
+    HomeComponent,
+    HomeNewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularEditorModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
